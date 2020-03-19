e2f6e24a790c25c0a2dd6b4c54aabb1d07a9577e
Julian Petersen <juli11c0@edu.ucl.dk>
2020-03-19 15:27:40 +0100

    login design with bootstrap!!

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,19 @@
 <template>
 <div class="login">
-    <h1>this is the login screen</h1>
-  <div class="container">
-      <form  class="loginform">
-          <input v-model="username" type="text" placeholder="username">
-          <input v-model="password" type="password" placeholder="password">
-          <input type="submit" value="login">
-      </form>
-  </div>
+    <h1>Spin-Robotics</h1>
+    <div class="container mt3">
+        <form  class="loginform form-group">
+            <!-- form controll makes the nice wide textbox around the input -->
+            <input class="inputform form-control mb-3" v-model="username" type="text" placeholder="username">
+            <input class="inputform form-control mb-3" v-model="password" type="password" placeholder="password">
+           <span class="logingspan">
+            <input type="checkbox" name="checkbox" id="">
+            <label class="pl-2 pr-3" for="checkbox">forbliv logget ind</label>
+            <input class="inputbtn btn-primary rounded" type="submit" value="login">
+           </span>
+        </form>
+    </div>
+
 </div>
 </template>
 
@@ -33,8 +39,28 @@ export default {
     .loginform{
         display:flex;
         flex-direction:column;
-        width:200px;
+        width:300px;
         margin:auto;
+    }
+
+    .container{
+        margin-top:50px;
+    }
+
+    .inputform{
+        border-color: #49b1ec;
+    }
+    
+    .inputbtn{
+        background-color: #49b1ec;
+        border-color: #49b1ec;
+        width:40%;
+        /* because the container is flex i can align this "self" to the end */
+        align-self:flex-end;
+    }
+
+    .loginspan{
+
     }
 
 </style>
