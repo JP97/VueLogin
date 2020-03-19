ae74dc894e27e220c5424df66ba4459a167be6e6
Julian Petersen <juli11c0@edu.ucl.dk>
2020-03-19 14:29:35 +0100

    imported bootstrap

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
   </div>
 </template>
 
+
 <script>
 import login from './components/Login.vue';
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
 import store from './store/index';
+//to install bootstrap run this is the commetn section: 
+// npm install bootstrap-vue bootstrap --save
+
+//import bootstrap
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+// Import the styles directly. (Or you could add them via script tags.)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+//then use it with Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
